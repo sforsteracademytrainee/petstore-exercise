10672663bc9b3414513c071bc986a6a3819f52de
Carl Angeles <CAngeles@academytrainee.com>
2021-01-15 12:19:28 +0000

    CA: added view by status

--- viewPets.js
+++ viewPets.js
@@ -1,5 +1,9 @@
-const viewPets = document.querySelector("#viewPets");
+const viewPetsByID = document.querySelector("#viewPetsByID");
 const petID = document.querySelector("#petID");
+const pets = document.querySelector("#pets");
+const status = document.querySelector("#status");
+const viewPetsByStatus = document.querySelector("#viewPetsByStatus");
+
 
 const viewPetByID = () =>
 {
@@ -19,11 +23,49 @@ const viewPetByID = () =>
             response.json().then(json =>
                 {
                     console.log(json);
-                    console.log(json.data);
+                    // for (let i = 0; i < json.length; i++)
+                    // {
+                    //     let p = document.createElement("p");
+                    //     p.className = "pets";
+
+                    //     let info = document.createTextNode(json[i].name);
+
+                    //     p.appendChild(info);
+                    //     pets.appendChild(p);
+                    // }
+                    
                 })
         }
     })
     .catch(err => console.error(`Stop! ${err}`));
 }
 
-viewPets.addEventListener('click', viewPetByID);
+const viewPetByStatus = () =>
+{
+    const petStatus = status.value;
+    fetch(`https://petstore.swagger.io/v2/pet/findByStatus?status=${petStatus}`)
+    .then((response) =>
+    {
+        console.log(response);
+        if (response.status !== 200)
+        {
+            console.error(`Looks like there was a problem. Status Code: ` +
+            response.status);
+            return;
+        }
+        else
+        {
+            response.json().then(json =>
+                {
+                    console.log(json);
+                    console.log(json.data);
+                    let p = document.createElement("p");
+                    
+                })
+        }
+    })
+    .catch(err => console.error(`Stop! ${err}`));
+}
+
+viewPetsByID.addEventListener('click', viewPetByID);
+viewPetsByStatus.addEventListener(`click`, viewPetByStatus);
